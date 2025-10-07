3153073d0d342186066b56e8abf022941628e396
JCDacuma <jauiedacumacanon@gmail.com>
2025-10-07 21:12:59 +0800

    feat(api): implement account CRUD and registration features

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use App\Models\User;
 
 class UserController extends Controller
 {
-    public function index(){
-        $user = User::all();
-        return response()->json($user);
-    }
+    public function index()
+{
+    $users = User::with('role')->get()->map(function ($user) {
+        return [
+            'id' => $user->id,
+            'firstname' => $user->firstname,
+            'lastname' => $user->lastname,
+            'username' => $user->username,
+            'phonenumber' => $user->phonenumber,
+            'email' => $user->email,
+            'account_status' => $user->account_status,
+            'role_id' => $user->role_id,
+            'role_name' => $user->role->role_name ?? null, 
+        ];
+    });
+
+    return response()->json($users);
+}
 
     //check email availability
     public function checkemail(Request $request){
@@ -54,5 +69,30 @@ class UserController extends Controller
         ]);
 
         return response()->json(['message' => 'user has successfully registered','user' => $user],201);
+    }
+
+
+    //update user account
+    public function update(Request $request, string $id){
+           $user = User::findOrFail($id);
+
+           $validated = $request->validate([
+          'firstname'=>'sometimes|string|max:255',
+          'lastname' => 'sometimes|string|max:255',
+          'username' => ['sometimes|string', Rule::unique('users' , 'username')->ignore($user->id)],
+          'email' => ['sometimes|string', Rule::unique('users','email')->ignore($user->id)],
+          'role_id' => 'sometimes|integer|exists:roles,id',
+          'phonenumber' => ['sometimes', 'regex:/^(?:\+?63|0)9\d{9}$/'],
+          'password' => 'sometimes|string|min:8',
+          'account_status' => 'sometimes|string|max:255'
+        ]);
+
+        if(isset($validated['password'])){
+            $validated['password'] = bcrypt($validated['password']);
+        }
+
+        $user->update($validated);
+
+        return response()->json(['message'=> 'user successfully updated', 'user' => $user], 200);
     }
 }
