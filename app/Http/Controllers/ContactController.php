771fb075142ed6313b0367f5a2316d273604cec7
JCDacuma <jauiedacumacanon@gmail.com>
2025-10-10 15:05:26 +0800

    (feat): add update for supplier and contact table

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -18,16 +18,39 @@ class ContactController extends Controller
             'firstname'=>'required|string|max:255',
             'lastname'=>'required|string|max:255',
             'phonenumber'=> ['required', 'regex:/^(?:\+?63|0)9\d{9}$/'],
-            'email'=>'required|string|unique:supplier_contacts,email',
+            'email'=>'required|email|unique:supplier_contacts,email',
         ]);
 
-        $user = SupplierContact::create([
+        $contact = SupplierContact::create([
             'firstname'=>$validated['firstname'],
             'lastname'=>$validated['lastname'],
             'phonenumber'=>$validated['phonenumber'],
             'email'=>$validated['email'],
         ]);
 
-        return response()->json(['message' => 'successfully registered new user' , 'user' => $user ], 201);
+        return response()->json(['message' => 'successfully registered new user' , 'contact' => $contact ], 201);
+    }
+
+    
+    public function update(Request $request, SupplierContact $contact){
+       
+
+        $validated = $request->validate([
+            'firstname'=>'sometimes|string|max:255',
+            'lastname'=>'sometimes|string|max:255',
+            'phonenumber'=> ['sometimes', 'regex:/^(?:\+?63|0)9\d{9}$/'],
+            'email'=>'sometimes|email|unique:supplier_contacts,email,'. $contact->id
+        ]);
+
+        $contact->update([
+            'firstname'=>$validated['firstname'] ?? $contact->firstname,
+            'lastname'=>$validated['lastname'] ?? $contact->lastname,
+            'phonenumber'=>$validated['phonenumber'] ?? $contact->phonenumber,
+            'email'=>$validated['email'] ?? $contact->email,
+        ]);
+
+        return response()->json([
+            'message' => 'Contact Successfully Registered', 'Contact' => $contact
+        ], 200);
     }
 }
--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -70,7 +70,25 @@ class SupplierController extends Controller
      */
     public function update(Request $request, Supplier $supplier)
     {
-        //
+        $validated = $request->validate([
+            'suppliername'=>'sometimes|string|max:255',
+            'supplier_address'=>'sometimes|string|max:255',
+            'shipping_fee'=>'sometimes|numeric',
+            'vat_registered'=>'sometimes|boolean',
+            'supplier_contact_id' => 'sometimes|integer|exists:supplier_contacts,id',
+            'status'=>'sometimes|string|max:255'
+        ]);
+
+        $supplier->update([
+            'suppliername'=>$validated['suppliername'] ?? $supplier->suppliername,
+            'supplier_address'=>$validated['supplier_address'] ?? $supplier->supplier_address,
+            'shipping_fee'=>$validated['shipping_fee'] ?? $supplier->shipping_fee,
+            'vat_registered'=>$validated['vat_registered'] ?? $supplier->vat_registered,
+            'supplier_contact_id'=>$validated['supplier_contact_id'] ?? $supplier->supplier_contact_id,
+            'status'=>$validated['status'] ?? $supplier->status,
+        ]);
+
+        return response()->json(['message' => 'Supplier Successfully updated', 'supplier' => $supplier ], 200);
     }
 
     /**
